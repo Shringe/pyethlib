a58a7264ede7d8afb3309a4d184802876fa1246c
Shringe <dashingkoso@gmail.com>
2026-02-20 16:11:00 -0600

    added requirements.txt

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-25.11";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -15,25 +13,30 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+        pkgs = import nixpkgs { inherit system; };
+
+        pythonEnv = pkgs.python313.withPackages (
+          pypkgs: with pypkgs; [
+            google-cloud-bigquery
+            packaging # Needed for bigquery
+            whenever
+          ]
+        );
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            # google-cloud-sdk
-            sqlitebrowser
-
-            (pkgs.python313.withPackages (
-              pypkgs: with pypkgs; [
-                google-cloud-bigquery
-                packaging # Needed for bigquery
-                whenever
-              ]
-            ))
+          buildInputs = [
+            pkgs.sqlitebrowser
+            pythonEnv
           ];
         };
+
+        packages.gen-requirements = pkgs.writeShellScriptBin "gen-requirements" ''
+          ${pkgs.python313Packages.pip}/bin/pip freeze --path \
+            ${pythonEnv}/lib/python3.13/site-packages \
+            > requirements.txt \
+            && echo "Written to requirements.txt"
+        '';
       }
     );
 }
